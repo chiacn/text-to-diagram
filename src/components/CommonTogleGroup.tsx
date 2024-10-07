21a47322d17b3849847f9dd94737a0c91a19a597
chiacn <chiacn@naver.com>
2024-10-07 16:22:36 +0900

    ui: CommonTogleGroup 컴포넌트 버튼 클릭 시 background 변경 안 되는 현상 수정

--- src/components/CommonTogleGroup.tsx
+++ src/components/CommonTogleGroup.tsx
@@ -28,12 +28,10 @@ export default function CommonToggleGroups({
           key={item.value}
           value={item.value}
           className={`
-            px-4 py-2 border border-gray-300 rounded 
-            bg-white hover:bg-gray-100 transition-colors 
-            cursor-pointer min-h-16
-            ${
-              selectedValue === item.value ? "bg-gray-100" : "hover:bg-gray-100"
-            } 
+            px-4 py-2 border border-gray-300 rounded
+            hover:bg-gray-100 transition-colors 
+            cursor-pointer min-h-16 hover:bg-gray-100 
+            ${selectedValue === item.value ? "bg-gray-100" : "bg-white"}
           `}
         >
           {item.label}
