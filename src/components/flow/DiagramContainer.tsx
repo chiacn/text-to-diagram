0285ba27691c42f672b4e11179418ae1f2d37fd9
chiacn <chiacn@naver.com>
2024-10-31 17:42:11 +0900

    refactor: SubmittedText 컴포넌트화해서 분리

--- src/components/flow/DiagramContainer.tsx
+++ src/components/flow/DiagramContainer.tsx
@@ -5,6 +5,7 @@ import useLLM from "@/commonHooks/useLLM";
 import CommonToggleGroups from "../CommonTogleGroup";
 import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
+import SubmittedText from "./SubmittedText";
 
 export default function DiagramContainer() {
   // LLM 테스트 ---------------------------------------------
@@ -501,45 +502,11 @@ export default function DiagramContainer() {
             Submit
           </Button>
         </div>
-
-        {/* Submit된 텍스트 표시 영역 */}
-        <div
-          className={`w-full mt-8 bg-white border border-gray-300 rounded-lg shadow-lg overflow-hidden ${
-            submittedText
-              ? "opacity-100 max-h-[500px] scale-100"
-              : "opacity-0 max-h-0 scale-95"
-          }`}
-        >
-          {/* 상단 바 -------------------------------------------------- */}
-          <div className="flex items-center justify-end p-2 bg-gray-100 border-b border-gray-300">
-            {/* 접기/펼치기 토글 버튼 */}
-            <button
-              onClick={() => setIsOpenSubmittedText(!isOpenSubmittedText)}
-              className="text-gray-600 hover:text-gray-800 transition-transform duration-300 transform"
-            >
-              <span
-                className={`inline-block transition-transform duration-300 ${
-                  isOpenSubmittedText ? "rotate-180" : "rotate-0"
-                }`}
-              >
-                ▲
-              </span>
-            </button>
-          </div>
-          {/* ----------------------------------------------------------- */}
-
-          {/* 제출된 텍스트 표시 영역 */}
-          <div
-            className={`transition-all duration-500 transform whitespace-pre-wrap text-left p-4 ${
-              isOpenSubmittedText
-                ? "opacity-100 max-h-[500px] scale-100"
-                : "opacity-0 max-h-0 scale-95"
-            }`}
-            style={{ overflow: "hidden" }}
-          >
-            {submittedText}
-          </div>
-        </div>
+        <SubmittedText
+          submittedText={submittedText}
+          isOpenSubmittedText={isOpenSubmittedText}
+          setIsOpenSubmittedText={setIsOpenSubmittedText}
+        />
       </div>
 
       {/* 상단 스크롤 ---------------------------------------------------- */}
