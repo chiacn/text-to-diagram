d1b1b74e809a60e5620caafae968fb6f0ede4890
chiacn <chiacn@naver.com>
2025-05-05 14:32:37 +0900

    refactor:  hook - useCallback, useMemo 적용

--- src/components/flow/hooks/useHighlight.ts
+++ src/components/flow/hooks/useHighlight.ts
@@ -1,4 +1,5 @@
-import { useEffect, useRef, useState } from "react";
+import { COLOR_PALETTE } from "@/constants";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 interface UseHighlightProps {
   inquiryType: string | null;
@@ -20,103 +21,106 @@ export default function useHighlight({ inquiryType }: UseHighlightProps) {
   const [currentHighlightStatus, setCurrentHighlightStatus] = useState<{
     value: number;
   }>({ value: 0 });
-  const colorPalette = ["#E3FDFD", "#D4F3EF", "#F9E2E3", "#F0F7EE", "#D2E4F2"];
+  const colorPalette = COLOR_PALETTE;
   const targetColorMap = useRef<any>({});
 
-  const handleDiagramItem = (
-    effectType: string,
-    params: {
-      diagramId: number | string;
-      depth: number;
-      parentDiagramId?: number | string;
-    },
-  ) => {
-    switch (effectType) {
-      // depth 기준으로 highlight
-      case "highlight":
-        // * inquiryType이 logical_progression일 경우, highlightStatus에 따라 highlight 종류를 여러개 두는게 아니라 해당 step만 highlight되도록 변경.
-        if (inquiryType === "logical_progression") {
-          let newHighlightItems;
-          if (highlightItems.includes(params.diagramId)) {
-            // 이미 하이라이트된 경우 제거
-            newHighlightItems = highlightItems.filter(
-              (id) => id !== params.diagramId,
-            );
-          } else {
-            // 선택한 diagramId 추가
-            newHighlightItems = [...highlightItems, params.diagramId];
+  const handleDiagramItem = useCallback(
+    (
+      effectType: string,
+      params: {
+        diagramId: number | string;
+        depth: number;
+        parentDiagramId?: number | string;
+      },
+    ) => {
+      switch (effectType) {
+        // depth 기준으로 highlight
+        case "highlight":
+          // * inquiryType이 logical_progression일 경우, highlightStatus에 따라 highlight 종류를 여러개 두는게 아니라 해당 step만 highlight되도록 변경.
+          if (inquiryType === "logical_progression") {
+            let newHighlightItems;
+            if (highlightItems.includes(params.diagramId)) {
+              // 이미 하이라이트된 경우 제거
+              newHighlightItems = highlightItems.filter(
+                (id) => id !== params.diagramId,
+              );
+            } else {
+              // 선택한 diagramId 추가
+              newHighlightItems = [...highlightItems, params.diagramId];
+            }
+            setHighlightItems(newHighlightItems);
+
+            // 선택된 아이템이 있을 때는 SEPARATE_COLOR, 없을 때는 NONE으로 설정
+            if (newHighlightItems.length > 0) {
+              setCurrentHighlightStatus({
+                value: HIGHLIGHT_STATUS.SEPARATE_COLOR,
+              });
+            } else {
+              setCurrentHighlightStatus({ value: HIGHLIGHT_STATUS.NONE });
+            }
+            return;
           }
-          setHighlightItems(newHighlightItems);
 
-          // 선택된 아이템이 있을 때는 SEPARATE_COLOR, 없을 때는 NONE으로 설정
-          if (newHighlightItems.length > 0) {
-            setCurrentHighlightStatus({
-              value: HIGHLIGHT_STATUS.SEPARATE_COLOR,
-            });
-          } else {
-            setCurrentHighlightStatus({ value: HIGHLIGHT_STATUS.NONE });
-          }
-          return;
-        }
+          const { parentDiagramId } = params;
+          // Get all diagramIds with matching parentDiagramId
+          const diagramIdsToHighlight = diagramItemsListRef.current
+            .filter((item) => item.parentDiagramId === parentDiagramId)
+            .map((item) => item.diagramId);
 
-        const { parentDiagramId } = params;
-        // Get all diagramIds with matching parentDiagramId
-        const diagramIdsToHighlight = diagramItemsListRef.current
-          .filter((item) => item.parentDiagramId === parentDiagramId)
-          .map((item) => item.diagramId);
-
-        if (
-          highlightItems.length > 0 &&
-          diagramIdsToHighlight.some((id) => !highlightItems.includes(id))
-        ) {
-          // 다른 diagram 선택 시 currentHighlightStatus 초기화
-          setCurrentHighlightStatus({ value: 0 });
-          setHighlightItems(
-            setHighlightItemsByStatus(0, diagramIdsToHighlight),
-          );
-        } else {
-          setCurrentHighlightStatus((prevHighlightStatus) => {
-            const newHighlightStatus = {
-              value: (prevHighlightStatus.value + 1) % 4,
-            };
+          if (
+            highlightItems.length > 0 &&
+            diagramIdsToHighlight.some((id) => !highlightItems.includes(id))
+          ) {
+            // 다른 diagram 선택 시 currentHighlightStatus 초기화
+            setCurrentHighlightStatus({ value: 0 });
             setHighlightItems(
-              setHighlightItemsByStatus(
-                newHighlightStatus.value,
-                diagramIdsToHighlight,
-              ),
+              setHighlightItemsByStatus(0, diagramIdsToHighlight),
             );
-            return newHighlightStatus;
-          });
-        }
+          } else {
+            setCurrentHighlightStatus((prevHighlightStatus) => {
+              const newHighlightStatus = {
+                value: (prevHighlightStatus.value + 1) % 4,
+              };
+              setHighlightItems(
+                setHighlightItemsByStatus(
+                  newHighlightStatus.value,
+                  diagramIdsToHighlight,
+                ),
+              );
+              return newHighlightStatus;
+            });
+          }
 
-        // * Note: 주의 currentHighlightStatus는 비동기적으로 업데이트되고, 다음 렌더링 사이클에 상태 업데이트를 적용하므로
-        // * currentHighlightStatus를 찍어도 업데이트 되지 않은 상태가 보여짐.
-        function setHighlightItemsByStatus(
-          status: number,
-          highlightDiagramIds: (string | number)[] = [],
-        ) {
-          return (
-            {
-              [HIGHLIGHT_STATUS.NONE]: [],
-              [HIGHLIGHT_STATUS.SAME_DEPTH]: [...highlightDiagramIds],
-              [HIGHLIGHT_STATUS.SEPARATE_COLOR]: [
-                ...highlightItems,
-                ...highlightDiagramIds,
-              ],
-              [HIGHLIGHT_STATUS.SINGLE_STEP]: [params.diagramId],
-            }[status] || []
-          );
-        }
-        break;
-      default:
-        break;
-    }
-  };
+          // * Note: 주의 currentHighlightStatus는 비동기적으로 업데이트되고, 다음 렌더링 사이클에 상태 업데이트를 적용하므로
+          // * currentHighlightStatus를 찍어도 업데이트 되지 않은 상태가 보여짐.
+          function setHighlightItemsByStatus(
+            status: number,
+            highlightDiagramIds: (string | number)[] = [],
+          ) {
+            return (
+              {
+                [HIGHLIGHT_STATUS.NONE]: [],
+                [HIGHLIGHT_STATUS.SAME_DEPTH]: [...highlightDiagramIds],
+                [HIGHLIGHT_STATUS.SEPARATE_COLOR]: [
+                  ...highlightItems,
+                  ...highlightDiagramIds,
+                ],
+                [HIGHLIGHT_STATUS.SINGLE_STEP]: [params.diagramId],
+              }[status] || []
+            );
+          }
+          break;
+        default:
+          break;
+      }
+    },
+    [inquiryType],
+  );
 
-  const resetHighlight = () => {
+  const resetHighlight = useCallback(() => {
     setHighlightItems([]);
     setCurrentHighlightStatus({ value: 0 });
-  };
+  }, []);
 
   // Reset diagramItemsListRef before rendering
   diagramItemsListRef.current = [];
